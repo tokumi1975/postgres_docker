dea3265523d370a92313f8a6b6853a2e04e0bc10
tateki tokumi <1093.tt@gmail.com>
2020-10-06 11:39:18 +0900

    サンプルデータベースに関するクエリを追加

--- initdb/1_createdb.sh
+++ initdb/1_createdb.sh
@@ -1,4 +1,15 @@
 psql << EOSQL
   CREATE DATABASE testdb;
   GRANT ALL PRIVILEGFES ON DATABASE testdb TO postgres;
+  CREATE DATABASE sampledb;
+  \c sampledb
+  CREATE TABLE users (
+    id integer NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255),
+    phone VARCHAR(50),
+    update_date TIMESTAMP,
+    create_date TIMESTAMP,
+    delete_date TIMESTAMP,
+    PRIMARY KEY (id));
 EOSQL
--- initdb/2_createdata.sh
+++ initdb/2_createdata.sh
@@ -8,4 +8,6 @@ COPY testtable (
   id,
   value
 ) FROM '/docker-entrypoint-initdb.d/initdata.csv' WITH CSV;
+\c sampledb
+INSERT INTO users (id, name, address, phone, update_date, create_date) VALUES ('1', 'テスト太郎', '東京都サンプル区1-1', '080-0000-0000', '2019-05-06 12:00:00', '2019-05-01 12:00:00');
 EOSQL
